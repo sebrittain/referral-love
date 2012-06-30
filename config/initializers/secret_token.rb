748485e19003cb4a23bf3150c12b1ff741b13a5a
Sarah Brittain <sebrittain3@gmail.com>
2012-06-30 13:20:20 -0400

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReferralLove::Application.config.secret_token = '85348ecbdf0a0f847a7504bfc35ed0b3c9a7a58b0c0fc2cf0ecf84a43f402eb9a4672be772e5b90a3d30d5f389f178d77bc29a194d853888a509741e2de2186d'
