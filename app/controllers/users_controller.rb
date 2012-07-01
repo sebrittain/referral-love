c8970d7a2bc6b33bda8a0f6ddec2366a4b1c3c8e
Sarah Brittain <sebrittain3@gmail.com>
2012-07-01 03:57:19 -0500

    sleep deprived design and stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,43 @@
 class UsersController < ApplicationController
+  def index
+  	 @user = User.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @user }
+    end
+  end
   def new
   	@user = User.new
 
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @user }
+    end
+  end
+
+  def show
+  	@user = User.find(params[:id])
+
+  	respond_to do |format|
+  		format.html
+  		format.json {render json: @user }
+  	end
+end
+def edit 
+	@user = User.find(params[:id])
+end
+ def create
+    @user = User.new(params[:user])
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User created' }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ReferralLove::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/successful"
+  get "static_pages/help"
+
   resources :users
 
   resources :referral_recipients
