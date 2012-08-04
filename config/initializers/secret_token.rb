8041f745ed61ed1da1d11f027771d2c0ad90abe9
aaronroe <aaronalkan@gmail.com>
2012-08-03 19:15:59 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReferralLove::Application.config.secret_token = '8c1a41ef87f5d7a031ea10f88be22ba288b0ce194bfe0140b91cc8b85797826c7b4652eecccfd29c21e3f26c568a51b82cbfa45f7ef074cb475da7c5fbf34ae2'
